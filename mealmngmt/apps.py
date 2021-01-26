338189d08c846eda31e08a21107d0a62c4118c83
Shiroko <thothctm@gmail.com>
2021-01-26 19:35:55 -0300

    initial mode

---
+++ mealmngmt/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MealmngmtConfig(AppConfig):
+    name = 'mealmngmt'
---
+++ mealmngmt/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Actors
+class User(AbstractUser):
+    pass
+
+
+class MealManager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Employe(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+
+# Entities
+
+
+class Menu(models.Model):
+    message = models.CharField(max_length=140)
+    date = models.DateField()
+    mealmanager = models.ForeignKey(MealManager, on_delete=models.CASCADE)
+
+
+class MenuRequest(models.Model):
+    option = models.CharField(max_length=40),
+    customization = models.CharField(max_length=140)
+    employe = models.ForeignKey(Employe, on_delete=models.CASCADE)
+
+# xoxb-1683708563844-1690268597857-XS6OkIBmsqCkQ3qeWAQKvabr
